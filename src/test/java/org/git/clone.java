ddfb674e5b5887ddc32b54c1ca801f643686e706
vishnu0410 <vishnuboyapati851@gmail.com>
2021-08-03 13:53:58 +0530

    poornaaaaa

--- src/test/java/org/git/clone.java
+++ src/test/java/org/git/clone.java
@@ -1,5 +1,11 @@
 package org.git;
 
 public class clone {
-
-}
+	public static void username() {
+		// TODO Auto-generated method stub
+		System.out.println("userName");
+	}
+	public static void main(String[] args) {
+		username();
+	}
+} 
